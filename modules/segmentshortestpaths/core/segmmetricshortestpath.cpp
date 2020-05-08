08391b094636ecdb7515b00f3fe9b8d0313acea5
Petros Koutsolampros <commits@pklampros.io>
2020-05-08 12:14:24 +0100

    Bring the shortest paths code from the segment_shortest_paths branch

---
+++ modules/segmentshortestpaths/core/segmmetricshortestpath.cpp
@@ -0,0 +1,159 @@
+// sala - a component of the depthmapX - spatial network analysis platform
+// Copyright (C) 2000-2010, University College London, Alasdair Turner
+// Copyright (C) 2011-2012, Tasos Varoudis
+// Copyright (C) 2017-2018, Petros Koutsolampros
+
+// This program is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+#include "segmmetricshortestpath.h"
+
+#include "genlib/stringutils.h"
+
+bool SegmentMetricShortestPath::run(Communicator *comm, ShapeGraph &map, bool simple_version) {
+
+    AttributeTable &attributes = map.getAttributeTable();
+
+    bool retvar = true;
+
+    int dist_col = attributes.insertOrResetColumn("Metric Shortest Path Distance");
+    int path_col = attributes.insertOrResetColumn("Metric Shortest Path Order");
+
+    // record axial line refs for topological analysis
+    std::vector<int> axialrefs;
+    // quick through to find the longest seg length
+    std::vector<float> seglengths;
+    float maxseglength = 0.0f;
+    for (size_t cursor = 0; cursor < map.getShapeCount(); cursor++) {
+        AttributeRow &row = map.getAttributeRowFromShapeIndex(cursor);
+        axialrefs.push_back(row.getValue("Axial Line Ref"));
+        seglengths.push_back(row.getValue("Segment Length"));
+        if (seglengths.back() > maxseglength) {
+            maxseglength = seglengths.back();
+        }
+    }
+
+    int maxbin = 512;
+
+    std::vector<unsigned int> seen(map.getShapeCount());
+    std::vector<TopoMetSegmentRef> audittrail(map.getShapeCount());
+    std::vector<int> list[512]; // 512 bins!
+    int open = 0;
+
+    for (size_t i = 0; i < map.getShapeCount(); i++) {
+        seen[i] = 0xffffffff;
+    }
+    auto &selected = map.getSelSet();
+    if (selected.size() != 2) {
+        return false;
+    }
+    int refFrom = *selected.begin();
+    int refTo = *selected.rbegin();
+
+    seen[refFrom] = 0;
+    open++;
+    double length = seglengths[refFrom];
+    audittrail[refFrom] = TopoMetSegmentRef(refFrom, Connector::SEG_CONN_ALL, length * 0.5, -1);
+    // better to divide by 511 but have 512 bins...
+    list[(int(floor(0.5 + 511 * length / maxseglength))) % 512].push_back(refFrom);
+    map.getAttributeRowFromShapeIndex(refFrom).setValue(dist_col, 0);
+
+    unsigned int segdepth = 0;
+    int bin = 0;
+
+    std::map<unsigned int, unsigned int> parents;
+    bool refFound = false;
+
+    while (open != 0) {
+        while (list[bin].size() == 0) {
+            bin++;
+            segdepth += 1;
+            if (bin == maxbin) {
+                bin = 0;
+            }
+        }
+        //
+        TopoMetSegmentRef &here = audittrail[list[bin].back()];
+        list[bin].pop_back();
+        open--;
+        // this is necessary using unsigned ints for "seen", as it is possible to add a node twice
+        if (here.done) {
+            continue;
+        } else {
+            here.done = true;
+        }
+
+        Connector &axline = map.getConnections().at(here.ref);
+        int connected_cursor = -2;
+
+        auto iter = axline.m_back_segconns.begin();
+        bool backsegs = true;
+
+        while (connected_cursor != -1) {
+            if (backsegs && iter == axline.m_back_segconns.end()) {
+                iter = axline.m_forward_segconns.begin();
+                backsegs = false;
+            }
+            if (!backsegs && iter == axline.m_forward_segconns.end()) {
+                break;
+            }
+
+            connected_cursor = iter->first.ref;
+            if (seen[connected_cursor] > segdepth) {
+                float length = seglengths[connected_cursor];
+                seen[connected_cursor] = segdepth;
+                audittrail[connected_cursor] =
+                    TopoMetSegmentRef(connected_cursor, here.dir, here.dist + length, here.ref);
+                parents[connected_cursor] = here.ref;
+                // puts in a suitable bin ahead of us...
+                open++;
+                //
+                // better to divide by 511 but have 512 bins...
+                list[(bin + int(floor(0.5 + 511 * length / maxseglength))) % 512].push_back(connected_cursor);
+                AttributeRow &row = map.getAttributeRowFromShapeIndex(connected_cursor);
+                row.setValue(dist_col, here.dist + length * 0.5);
+            }
+            if (connected_cursor == refTo) {
+                refFound = true;
+                break;
+            }
+            iter++;
+        }
+        if (refFound)
+            break;
+    }
+
+    auto refToParent = parents.find(refTo);
+    int counter = 0;
+    while (refToParent != parents.end()) {
+        AttributeRow &row = map.getAttributeRowFromShapeIndex(refToParent->first);
+        row.setValue(path_col, counter);
+        counter++;
+        refToParent = parents.find(refToParent->second);
+    }
+    map.getAttributeRowFromShapeIndex(refFrom).setValue(path_col, counter);
+
+    for (auto iter = attributes.begin(); iter != attributes.end(); iter++) {
+        AttributeRow &row = iter->getRow();
+        if (row.getValue(path_col) < 0) {
+            row.setValue(dist_col, -1);
+        } else {
+            row.setValue(path_col, counter - row.getValue(path_col));
+        }
+    }
+
+    map.overrideDisplayedAttribute(-2);
+    map.setDisplayedAttribute(path_col);
+
+    return retvar;
+}
