91eb963e07183aabc4a03d495567167495e02c81
Petros Koutsolampros <commits@pklampros.io>
2020-05-15 21:16:14 +0100

    Add moduleTest to the travis config

--- ci/build.sh
+++ ci/build.sh
@@ -10,7 +10,7 @@ echo Building
 cmake -DCMAKE_BUILD_TYPE=Release .. && cmake --build . || exit 1
 # if succeeds, run unit tests
 echo Running unit tests
-./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./mgraph440Test/mgraph440Test && ./salaTest/salaTest && ./genlibTest/genlibTest && ./depthmapXTest/depthmapXTest || exit 1
+./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./mgraph440Test/mgraph440Test && ./salaTest/salaTest && ./genlibTest/genlibTest && ./depthmapXTest/depthmapXTest && ./moduleTest/moduleTest || exit 1
 # if that succeeds, run regression tests
 echo testing regression test framework
 cd ../RegressionTest/test && echo pwd && python3.5 -u test_main.py || exit 1
