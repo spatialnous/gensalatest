6debde7cb2fd7349f5fb70dee6a7fd7a754f88e8
Petros Koutsolampros <commits@pklampros.io>
2020-05-17 20:25:20 +0100

    Provide regression tests within the module

--- ci/build.sh
+++ ci/build.sh
@@ -18,3 +18,10 @@ echo running standard regression tests
 cd .. && pwd && python3.5 -u RegressionTestRunner.py || exit 1
 echo running agent test
 python3.5 -u RegressionTestRunner.py regressionconfig_agents.json || exit 1
+# search the modules directory for regression tests and run them
+for subdir in ../modules/*/; do
+    regressionFile="${subdir}RegressionTest/regressionconfig.json"
+    if [ -e "$regressionFile" ]; then
+        python3.5 -u RegressionTestRunner.py "${regressionFile}" || exit 1
+    fi 
+done
