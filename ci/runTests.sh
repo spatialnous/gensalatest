1267aff19c27f2d142ba1947ba73df1b9ae72db8
Petros Koutsolampros <commits@pklampros.io>
2024-06-05 15:47:54 +0300

    Remove mgraph440 from testing

--- ci/runTests.sh
+++ ci/runTests.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 echo Running unit tests
-./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./mgraph440Test/mgraph440Test && ./salaTest/salaTest && ./genlibTest/genlibTest && ./depthmapXTest/depthmapXTest && ./moduleTest/moduleTest || exit 1
+./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./salaTest/salaTest && ./genlibTest/genlibTest && ./depthmapXTest/depthmapXTest && ./moduleTest/moduleTest || exit 1
 # if that succeeds, run regression tests
 echo testing regression test framework
 cd ../RegressionTest/test && echo pwd && python3.5 -u test_main.py || exit 1
