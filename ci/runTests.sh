674e4706c72fc1b41d7109355736b873f74a5b7b
Petros Koutsolampros <commits@pklampros.io>
2024-06-05 16:39:56 +0300

    Use python3 instead of python3.5

--- ci/runTests.sh
+++ ci/runTests.sh
@@ -4,15 +4,15 @@ echo Running unit tests
 ./cliTest/cliTest && ./GuiUnitTest/GuiUnitTest && ./salaTest/salaTest && ./genlibTest/genlibTest && ./depthmapXTest/depthmapXTest && ./moduleTest/moduleTest || exit 1
 # if that succeeds, run regression tests
 echo testing regression test framework
-cd ../RegressionTest/test && echo pwd && python3.5 -u test_main.py || exit 1
+cd ../RegressionTest/test && echo pwd && python3 -u test_main.py || exit 1
 echo running standard regression tests
-cd .. && pwd && python3.5 -u RegressionTestRunner.py || exit 1
+cd .. && pwd && python3 -u RegressionTestRunner.py || exit 1
 echo running agent test
-python3.5 -u RegressionTestRunner.py regressionconfig_agents.json || exit 1
+python3 -u RegressionTestRunner.py regressionconfig_agents.json || exit 1
 # search the modules directory for regression tests and run them
 for subdir in ../modules/*/; do
     regressionFile="${subdir}RegressionTest/regressionconfig.json"
     if [ -e "$regressionFile" ]; then
-        python3.5 -u RegressionTestRunner.py "${regressionFile}" || exit 1
+        python3 -u RegressionTestRunner.py "${regressionFile}" || exit 1
     fi 
 done
