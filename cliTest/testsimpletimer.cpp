8be8851144bf44ce7be1ebf6939af216875dd198
Petros Koutsolampros <commits@pklampros.io>
2024-07-18 15:40:20 +0300

    Switch testsimpletimer to the new Catch syntax

--- cliTest/testsimpletimer.cpp
+++ cliTest/testsimpletimer.cpp
@@ -12,17 +12,17 @@
 TEST_CASE("TestSimpleTimer", "") {
     SimpleTimer timer;
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    REQUIRE(timer.getTimeInSeconds() == Catch::Approx(0.5).epsilon(0.2));
+    REQUIRE_THAT(timer.getTimeInSeconds(), Catch::Matchers::WithinAbs(0.5, 0.2));
 }
 
 TEST_CASE("TestSimpleTimerReset", "") {
     SimpleTimer timer1;
     SimpleTimer timer2;
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    REQUIRE(timer1.getTimeInSeconds() == Catch::Approx(0.5).epsilon(0.2));
-    REQUIRE(timer2.getTimeInSeconds() == Catch::Approx(0.5).epsilon(0.2));
+    REQUIRE_THAT(timer1.getTimeInSeconds(), Catch::Matchers::WithinAbs(0.5, 0.2));
+    REQUIRE_THAT(timer2.getTimeInSeconds(), Catch::Matchers::WithinAbs(0.5, 0.2));
     timer2.reset();
     std::this_thread::sleep_for(std::chrono::milliseconds(500));
-    REQUIRE(timer1.getTimeInSeconds() == Catch::Approx(1.0).epsilon(0.2));
-    REQUIRE(timer2.getTimeInSeconds() == Catch::Approx(0.5).epsilon(0.2));
+    REQUIRE_THAT(timer1.getTimeInSeconds(), Catch::Matchers::WithinAbs(1.0, 0.2));
+    REQUIRE_THAT(timer2.getTimeInSeconds(), Catch::Matchers::WithinAbs(0.5, 0.2));
 }
